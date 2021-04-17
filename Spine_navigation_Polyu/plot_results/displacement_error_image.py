a3b673ec7af307a75f1d8dfdbb8d309defb85945
maryviktory <maryviktory@gmail.com>
2021-04-17 13:23:19 +0800

    plot results

--- Spine_navigation_Polyu/plot_results/displacement_error_image.py
+++ Spine_navigation_Polyu/plot_results/displacement_error_image.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import butter, filtfilt
 
-def smooth(signal, N=30):
-    N = 2  # Filter order
+def smooth(signal, N=2):
+    # N = 2  # Filter order
     Wn = 0.05  # Cutoff frequency
     B, A = butter(N, Wn, output='ba')
     # Second, apply the filter
@@ -14,6 +14,7 @@ def smooth(signal, N=30):
 def plot_path(probability, X, Y,color_ext,labels = None) :
     print("Plotting path")
     fig = plt.figure()
+
     ax = fig.add_subplot(111)
     ax.set(xlim=(0, 640), ylim=(0, len(probability)))
 
@@ -37,58 +38,110 @@ def plot_path(probability, X, Y,color_ext,labels = None) :
 
         zs = zs + 1
     ax.plot(smooth(path), index, label="coordinate continious")
+    # fig2 = plt.figure()
+    # ax2 = plt.gca()
+    # ax2.set_aspect((320 / zs) * 5)
+    # ax2.set_xlim(0,640)
+    # ax2.plot(smooth(path), index,label = "X image coordinate")
+    # ax2.set_xlabel('X image smoothed')
+    # ax2.set_ylabel('step')
+    # ax.plot(320,0, 320,zs, 'g--', label='line 1', linewidth=2)
     t = np.linspace(0, zs, zs)
 
+    fig4 = plt.figure()
+    ax4 = plt.gca()
+    ax4.set_aspect((320 / len(probability)) * 5)
+    ax4.set_xlim(0, 640)
+    ax4.plot(smooth(path), index,label = "X image coordinate")
+    x = [320,320]
+    y = [0,len(probability)]
+    ax4.plot(x,y,'g--',label = "middle line")
+    ax4.set_xlabel('X image smoothed')
+    ax4.set_ylabel('step')
+
+    fig3 = plt.figure()
+    ax1 = plt.gca()  # you first need to get the axis handle
+    # ax1.set_aspect((320/zs)*5)
+    ax1.plot(index,smooth(abs(320-path)), label = "Delta X image")
+    ax1.set_ylim(0, 640/2)
+    ax1.set_ylabel('delta Xim')
+    ax1.set_xlabel('step')
 
     # ax1.plot(probability,t_p)
     ax.set_xlabel(' X coordinate')
     ax.set_ylabel(' step')
+    ax.legend()
+    ax1.legend()
+    ax4.legend()
+    # ax2.legend()
     plt.show()
     return path
 
-folder = "2"
-data_path = "D:\spine navigation Polyu 2021\\robot_trials_output\phantom experiments\%s\Move_thread_output0.csv"%folder
+folder = "phantom experiments\scanning\median_filter_k21"
+data_path = "D:\spine navigation Polyu 2021\\robot_trials_output\%s\Move_thread_output0.csv"%folder
 
 frame = pd.read_csv(data_path)
 
-print(frame["X"])
+# print(frame["X"])
 X_mid = 640/2
 X_im = frame["X_im"]
 Y_im = frame["Y_im"]
 timestamp = frame["timestamp"] - frame["timestamp"][0]
-Y_robot = frame["Y_tcp"]
-X_robot = frame ["X_tcp"]
+Y_robot = frame["Y"]
+X_robot = frame ["X"]
+Z_robot = frame["Z"]
 X_force = frame["Fx"]
 Y_force = frame["Fy"]
 Z_force = frame["Fz"]
 frame_probability = frame["Frame_Probability"]
-
+X_tcp = frame["X_tcp"]
+Y_tcp = frame["Y_tcp"]
+Z_tcp = frame["Z_tcp"]
 delta_X = abs(X_im - X_mid)
 
+t = np.linspace(0, len(X_robot), len(X_robot))
 
+plt.figure()
+# plt.plot(timestamp,smooth(delta_X))
+ax = plt.gca() #you first need to get the axis handle
+ax.set_aspect(5*0.08/len(X_robot)) #sets the height to width ratio to 1.5.
+# plt.plot(smooth(-Y_robot),Z_robot)
+ax.plot(smooth(-X_tcp),t)
+ax.set_xlabel('X_tcp')
+ax.set_ylabel('step')
+ax.set_xlim(-0.08,0.08)
+#
 
-fig = plt.figure(1)
-# plt.plot( timestamp,delta_X)
-# plt.plot(X_im,Y_robot)
 # plt.plot(X_robot, Y_robot)
 
-t = np.linspace(0, len(X_robot), len(X_robot))
-plt.plot(-smooth(X_robot),t)
-# fig2 = plt.figure(2)
+# plt.plot(-smooth(X_robot),t)
 
-# plt.plot(timestamp,smooth(Y_force))
-# plt.title(" Y force")
+plt.figure()
 
-# fig3 = plt.figure(3)
+plt.plot(t,smooth(Y_force))
+plt.title(" Y force")
 
-# plt.plot(timestamp,smooth(X_force))
-# plt.title(" X force")
+plt.figure()
 
-path = plot_path(frame_probability,X_im,Y_im,"r")
+plt.plot(t,smooth(X_force))
+plt.title(" X force")
 
-# fig4 = plt.figure(4)
+plt.figure()
+plt.plot(t,smooth(Z_force))
+plt.title(" Z force")
+
+plt.figure()
+plt.plot(t,smooth(frame_probability))
+plt.title(" Probability")
+
+# path = plot_path(frame_probability,X_im,Y_im,"r")
+
+
 
 # plt.plot(path,timestamp)
 # plt.title(" X_im")
+
+
+
 
 plt.show()
